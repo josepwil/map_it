70b0f75af054f66d88f701b472fc02d8a1618cb6
Joseph Williams <josephwilliamsjs@gmail.com>
2021-03-06 11:59:46 -0500

    creating users table/seed data

--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,11 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Seeding data ..."
+
+user = User.create([{ 
+  name: 'Brian', 
+  email: 'brian@test.com', 
+  password: 'test'
+  }])
