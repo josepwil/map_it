9a33da19e7823196de4ea0bd39b559f32d82ed5b
Joseph Williams <josephwilliamsjs@gmail.com>
2021-03-10 12:35:17 -0500

    adding new user methods

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,34 @@
 class Api::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def new
+  def index
+    @users = User.all
+       if @users
+          render json: {
+          users: @users
+       }
+      else
+          render json: {
+          status: 500,
+          errors: ['no users found']
+      }
+     end
   end
 
+def show
+   @user = User.find(params[:id])
+       if @user
+          render json: {
+          user: @user
+       }
+       else
+          render json: {
+          status: 500,
+          errors: ['user not found']
+        }
+       end
+  end
+  
   def create
     @user = User.new(user_params)
     if user.save
