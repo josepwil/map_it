9dd97f8a199c239ab990d1a2b1916837622ba925
Joseph Williams <josephwilliamsjs@gmail.com>
2021-03-15 12:36:18 -0400

    creating markers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_162837) do
+ActiveRecord::Schema.define(version: 2021_03_15_163442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,20 @@ ActiveRecord::Schema.define(version: 2021_03_15_162837) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "markers", force: :cascade do |t|
+    t.bigint "map_id", null: false
+    t.string "coords"
+    t.string "popup"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["map_id"], name: "index_markers_on_map_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password"
   end
 
+  add_foreign_key "markers", "maps"
 end
