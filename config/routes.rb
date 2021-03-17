03f1146133b59136a04a420309867ce71b50b827
Joseph Williams <josephwilliamsjs@gmail.com>
2021-03-17 11:51:57 -0400

    creating map routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do # /api/users   
     resources :sessions
-    resources :users
+    resources :users 
+    resources :maps 
     post '/login' => 'sessions#create'
     post '/logout' => 'sessions#destroy'
     get '/logged_in' => 'sessions#is_logged_in?'
